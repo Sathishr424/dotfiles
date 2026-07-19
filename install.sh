1311381b381b026effd6596ac5b031ce6949d9f7
Sathish <rsathishikumar3@gmail.com>
2026-07-19 19:29:52 +0530

    Updated install.sh to include powerlevel10k

--- install.sh
+++ install.sh
@@ -3,11 +3,13 @@
 echo "Setting things up..."
 
 sudo rm -rf ~/.tmux.conf
+sudo rm -rf ~/.p10k.zsh
 sudo rm -rf ~/.vimrc
 sudo rm -rf ~/.zshrc
 sudo rm -rf ~/.config/hypr/hyprland.lua
 
 sudo ln -s ~/Documents/dotfiles/.tmux.conf ~/.tmux.conf
+sudo ln -s ~/Documents/dotfiles/.p10k.zsh ~/.p10k.zsh
 sudo ln -s ~/Documents/dotfiles/.vimrc ~/.vimrc
 sudo ln -s ~/Documents/dotfiles/.zshrc ~/.zshrc
 sudo ln -s ~/Documents/dotfiles/hyprland.lua ~/.config/hypr/hyprland.lua
