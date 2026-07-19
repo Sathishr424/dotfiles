4a0657935feb1d253bb8841f9979dd32afe71946
Sathish <rsathishikumar3@gmail.com>
2026-07-19 22:01:17 +0530

    changes

--- install.sh
+++ install.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+set -e
+
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 echo "Setting things up..."
+echo "Dotfiles directory: $DOTFILES_DIR"
 
-sudo rm -rf ~/.tmux.conf
-sudo rm -rf ~/.p10k.zsh
-sudo rm -rf ~/.vimrc
-sudo rm -rf ~/.zshrc
-sudo rm -rf ~/.config/hypr/hyprland.lua
+rm -f ~/.tmux.conf
+rm -f ~/.p10k.zsh
+rm -f ~/.vimrc
+rm -f ~/.zshrc
+rm -f ~/.config/hypr/hyprland.lua
 
-sudo ln -s ~/Documents/dotfiles/.tmux.conf ~/.tmux.conf
-sudo ln -s ~/Documents/dotfiles/.p10k.zsh ~/.p10k.zsh
-sudo ln -s ~/Documents/dotfiles/.vimrc ~/.vimrc
-sudo ln -s ~/Documents/dotfiles/.zshrc ~/.zshrc
-sudo ln -s ~/Documents/dotfiles/hyprland.lua ~/.config/hypr/hyprland.lua
+ln -sf "$DOTFILES_DIR/.tmux.conf" ~/.tmux.conf
+ln -sf "$DOTFILES_DIR/.p10k.zsh" ~/.p10k.zsh
+ln -sf "$DOTFILES_DIR/.vimrc" ~/.vimrc
+ln -sf "$DOTFILES_DIR/.zshrc" ~/.zshrc
+ln -sf "$DOTFILES_DIR/hyprland.lua" ~/.config/hypr/hyprland.lua
+
+echo "Done!"
