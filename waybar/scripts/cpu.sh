64c4c1e122514046bb6dd9cffd88b09292d7a8b9
Sathish <rsathishikumar3@gmail.com>
2026-07-22 00:01:06 +0530

    changes

--- waybar/scripts/cpu.sh
+++ waybar/scripts/cpu.sh
@@ -6,4 +6,4 @@ CPU_USAGE=$(top -bn1 | awk '/^%Cpu/ {printf "%.1f", 100 - $8}')
 CPU_FAN=$(sensors | awk '/fan1:/ {print $2; exit}')
 
 
-printf '{"text":"🔲 %s°C | %s%% | 𖣘 %s","tooltip":"CPU","class":"process-status "}\n' "$CPU_TEMP" "$CPU_USAGE" "$CPU_FAN"
+printf '{"text":"🔲 %s°C | %s%% | 𖣘 %s","class":"process-status "}\n' "$CPU_TEMP" "$CPU_USAGE" "$CPU_FAN"
--- waybar/scripts/ram.sh
+++ waybar/scripts/ram.sh
@@ -3,4 +3,4 @@
 RAM_USED=$(free -m | awk '/^Mem:/ {printf "%.1f", $3/1024}')
 RAM_TOTAL=$(free -m | awk '/^Mem:/ {printf "%.1f", $2/1024}')
 
-printf '{"text":"󰍛 %sGB / %sGB","tooltip":"CPU","class":"process-status "}\n' "$RAM_USED" "$RAM_TOTAL"
+printf '{"text":"󰍛 %sGB / %sGB","class":"process-status "}\n' "$RAM_USED" "$RAM_TOTAL"
